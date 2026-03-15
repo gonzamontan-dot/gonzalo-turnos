4861f9f073a68063fde57c63f128c76db5dd7b33
gonzamontan-dot <gonzamontan@gmail.com>
2026-03-14 21:39:26 -0300

    Refactor firebase configuration and imports

--- firebase.js
+++ firebase.js
@@ -1,14 +1,13 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
 const firebaseConfig = {
-apiKey: "TU_API_KEY",
-authDomain: "TU_DOMINIO.firebaseapp.com",
-projectId: "TU_PROYECTO",
-storageBucket: "TU_PROYECTO.appspot.com",
-messagingSenderId: "XXXXXXXX",
-appId: "XXXXXXXX"
+  apiKey: "TU_API_KEY",
+  authDomain: "TU_PROYECTO.firebaseapp.com",
+  projectId: "TU_PROYECTO",
+  storageBucket: "TU_PROYECTO.appspot.com",
+  messagingSenderId: "XXXX",
+  appId: "XXXX"
 };
 
 const app = initializeApp(firebaseConfig);
