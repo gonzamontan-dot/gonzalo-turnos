608c4e369b6a02b0af4d510ca819d239a8758d56
gonzamontan-dot <gonzamontan@gmail.com>
2026-03-14 21:24:23 -0300

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+
+const firebaseConfig = {
+apiKey: "TU_API_KEY",
+authDomain: "TU_DOMINIO.firebaseapp.com",
+projectId: "TU_PROYECTO",
+storageBucket: "TU_PROYECTO.appspot.com",
+messagingSenderId: "XXXXXXXX",
+appId: "XXXXXXXX"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
