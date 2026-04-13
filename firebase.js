313eb3eadf183fdefeca47e776a859d530e95a62
gonzamontan-dot <gonzamontan@gmail.com>
2026-04-12 23:39:53 -0300

    Add files via upload

--- firebase.js
+++ firebase.js
@@ -1,16 +1,1 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
-
-const firebaseConfig = {
-    apiKey: "AIzaSyCbFBto-0PhnVw3sdJOhpY6NI3OZ5lkBYQ",
-    authDomain: "gonzalo-turnos.firebaseapp.com",
-    projectId: "gonzalo-turnos",
-    storageBucket: "gonzalo-turnos.firebasestorage.app",
-    messagingSenderId: "739858386287",
-    appId: "1:739858386287:web:2ff5278cbf8d6dcdb12c9a",
-    measurementId: "G-4QQRSFYKD8"
-  };
-
-const app = initializeApp(firebaseConfig);
-
-export const db = getFirestore(app);
+// PEGAR CONFIG FIREBASE ACA
